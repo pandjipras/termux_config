a3a540953a7f8ed8e9f3cbe25476cbd96cb474ab
pandjipras <pandji.prasetya15@gmail.com>
2024-09-27 23:55:54 +0700

    menambahkan-file

---
+++ sync-github.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Path to your local Git repository
+REPO_PATH="/data/data/com.termux/files/home/termux_config"
+
+# Move to the repository directory
+cd $REPO_PATH
+
+# Copy the updated .zshrc file to the repository
+cp ~/.zshrc $REPO_PATH
+
+# Check if there are changes
+if [[ `git status --porcelain` ]]; then
+    git add .zshrc
+    git commit -m "Auto-update .zshrc"
+    git push origin
+else
+    echo "No changes detected in .zshrc"
+fi
